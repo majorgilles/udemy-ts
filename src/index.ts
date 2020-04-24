59e02644acbe7a5c19572043d57a02c97b3a983d
Gilles Major <major.gilles@gmail.com>
2020-04-24 15:03:26 +0200

    some play with imports / exports #2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import multiply, { multiplyByTwo } from "./multiply";
+import multiply, { multiplyByTwo as mBy2 } from "./multiply";
 import * as multiplyModule from "./multiply";
 const a = 2;
 const b = 500;
 
 console.log(`${a} * ${b}`);
-console.log(multiply(a, b));
+console.log(multiplyModule.default(a, b));
 
 console.log(`2 * ${a}`);
 console.log(multiplyModule.multiplyByTwo(a));
+console.log(mBy2(a));
