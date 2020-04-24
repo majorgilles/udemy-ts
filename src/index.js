285b7d40d6ddb46c79f56c82416c845209bc773c
Gilles Major <major.gilles@gmail.com>
2020-04-24 14:33:56 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+function multipliy(a, b) {
+    return a * b;
+}
+var a = 2;
+var b = 2;
+console.log(multipliy(a, b));
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function multipliy(a: number, b: number): number {
+  return a * b;
+}
+
+const a = 2;
+const b = 2;
+
+console.log(multipliy(a, b));
