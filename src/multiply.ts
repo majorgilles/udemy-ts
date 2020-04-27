b6c62b295b58a932aa2db779c6c5f9313dc9ff61
Gilles Major <major.gilles@gmail.com>
2020-04-27 15:41:06 +0200

    add multip

---
+++ src/multiply.ts
@@ -0,0 +1,9 @@
+export default function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+export interface HelloWorld {}
+
+export function multiplyByTwo(a: number) {
+  return multiply(a, 2);
+}
