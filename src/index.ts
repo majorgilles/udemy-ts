969f6c1fcd5fd4858f5538e31e13196ebf5d2aca
Gilles Major <major.gilles@gmail.com>
2020-04-28 17:20:04 +0200

    playing around with classes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 class Robot {
-  name: string;
+  private _color: string;
+  constructor(protected _name: string, color: string = "green") {
+    if (!Robot.isColorAvailable(color)) {
+      throw new Error(`Color ${color} not available.`);
+    }
+    this._color = color; // TODO how to avoid duplicating the setter code and still satisfy the compiler that _color is used?
+  }
 
-  constructor(name: string) {
-    this.name = name;
+  static availableColors = ["green", "yellow"];
+  static isColorAvailable(color: string) {
+    return Robot.availableColors.includes(color);
   }
 
   askName() {
@@ -12,8 +19,44 @@ class Robot {
   move(distance: number) {
     console.log(`${this.name} moved ${distance} meters`);
   }
+
+  set color(color: string) {
+    if (!Robot.isColorAvailable(color)) {
+      throw new Error(`Color ${color} not available.`);
+    }
+    this._color = color;
+  }
+
+  set name(value: string) {
+    this._name = `PREFIX_${value}`;
+  }
+
+  get name() {
+    return `PREFIX_${this._name}`;
+  }
 }
 
 const robot = new Robot("John");
 robot.askName();
 robot.move(21);
+robot.color = "green";
+//robot.color = "red"; crashes as expceted
+
+class FlyingRobot extends Robot {
+  private readonly jetpackSize: number;
+  constructor(name: string, color: string = "green", jetpackSize: number) {
+    super(name, color);
+    this.jetpackSize = jetpackSize;
+  }
+
+  move(distance: number) {
+    super.move(distance);
+    console.log(`${this.name} is flying ${distance} meters.`);
+  }
+}
+const flying_robot = new FlyingRobot("test", "green", 21);
+flying_robot.move(22);
+// console.log(`size: ${flying_robot.jetpackSize}`);
+//flying_robot.jetpackSize = 512;
+// console.log(`size: ${flying_robot.jetpackSize}`);
+console.log(`My name is ${flying_robot.name}`);
