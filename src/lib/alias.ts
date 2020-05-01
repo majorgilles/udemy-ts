877a2121d259431976a84b1fa1678c0af7c00146
Gilles Major <major.gilles@gmail.com>
2020-05-01 15:24:27 +0200

    type aliases

---
+++ src/lib/alias.ts
@@ -0,0 +1,7 @@
+type Alias1 = string | string[] | null;
+type Alias2 = { a: number } & { b: number };
+type Alias3<T> = T[];
+type Alias4 = {
+  a: number;
+  b: number;
+};
--- src/lib/intersetion.ts
+++ src/lib/intersetion.ts
@@ -14,13 +14,14 @@ function X(obj: IA & IB & IC) {
   return obj.a + obj.b + obj.c;
 }
 
-function combine<A, B>(objA: A, objB: B) A & B {
+function combine<A extends object, B extends object>(objA: A, objB: B): A & B {
   return {
     ...objA,
     ...objB,
   };
 }
 
-const objA = {a:1};
-const objB = {b: 2};
-const resultObj = combine(objA, objB)
+const objA = { a: 1 };
+const objB = { b: 2 };
+const resultObj = combine(objA, objB);
+//const resultObj2 = combine(1, objB); fails as expected because of the type specifications of A & B
