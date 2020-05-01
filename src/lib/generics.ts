1317c598e125859ac2f9d8c7d6aaad4414ee67ee
Gilles Major <major.gilles@gmail.com>
2020-05-01 14:44:02 +0200

    add ShoppingCart interface

--- src/lib/generics.ts
+++ src/lib/generics.ts
@@ -46,3 +46,23 @@ const getExpiredItems: GetExpiredItemsFunction = <Item extends Expirable>(
 
 const exp1 = getExpiredItems(chocoCakes);
 const exp2 = getExpiredItems(vanillaCakes);
+
+interface ShoppingCart<ItemId, Item> {
+  items: Array<Item>;
+  addItem(this: ShoppingCart<ItemId, Item>, item: Item): void;
+  getItemById(this: ShoppingCart<ItemId, Item>, id: ItemId): Item | undefined;
+}
+
+interface Item {
+  id: number;
+  price: number;
+}
+const cart: ShoppingCart<number, Item> = {
+  items: [],
+  addItem(item) {
+    this.items.push(item);
+  },
+  getItemById(id) {
+    return this.items.find((item) => item.id === id);
+  },
+};
